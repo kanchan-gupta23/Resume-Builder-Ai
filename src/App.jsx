74a64c08a91868167d4cc89dbe14a5efc7cbc756
kanchan-gupta23 <kanchangupta23102004@gmail.com>
2025-08-02 00:26:30 +0530

    #commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ResumeTemplate from "./ResumeTemplate";
+
+function App() {
+  return (
+    <div>
+      <ResumeTemplate />
+    </div>
+  );
+}
+
+export default App;
